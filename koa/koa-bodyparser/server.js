e23fa49195be244869ef5c931b56c05d539f7286
funnycoderstar <funnycoderstar@gmail.com>
2019-06-18 00:36:51 +0800

    feat: koa_cors源码解析

---
+++ koa/koa-bodyparser/server.js
@@ -0,0 +1,23 @@
+const Koa = require("koa");
+const app = new Koa();
+const route = require('koa-route');
+var bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
+
+app.use(bodyParser()); // 处理post请求的参数
+
+const login = ctx => {
+    const req = ctx.request.body;
+    const userName = req.userName;
+    ctx.response.body = {
+        data: {
+            userName,
+        },
+        msg: '登陆成功'
+    };
+}
+app.use(cors());
+app.use(route.post('/login', login));
+app.listen(3200, () => {
+    console.log('启动成功');
+});
